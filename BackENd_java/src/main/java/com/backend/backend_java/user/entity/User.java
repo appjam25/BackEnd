f8bb390eb6700b3b46423e9c102c404129268791
dkflfkd53 <b57062562@gmail.com>
2024-04-20 18:24:19 +0900

    user entity 추가

---
+++ BackENd_java/src/main/java/com/backend/backend_java/user/entity/User.java
@@ -0,0 +1,44 @@
+package com.backend.backend_java.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(name = "id", columnDefinition = "BINARY(16)", unique = true, nullable = false)
+    private UUID id = UUID.randomUUID();
+
+    @Column(name = "nickname", unique = true, nullable = false)
+    private String nickname;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "birth_date", nullable = false)
+    private LocalDateTime birthDate;
+
+    @Column(name = "profile_img_url")
+    private String profileImgUrl;
+}

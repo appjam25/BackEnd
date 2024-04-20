516d8a876171395a457dc6bddcf13395c4a4402a
dkflfkd53 <b57062562@gmail.com>
2024-04-20 18:28:18 +0900

    userRepository 추가

---
+++ BackENd_java/src/main/java/com/backend/backend_java/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.backend.backend_java.user.repository;
+
+import com.backend.backend_java.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+}

d6fd7014bb4e8c39eeaaa766460924cea951e363
dkflfkd53 <b57062562@gmail.com>
2024-04-20 18:28:29 +0900

    authController 추가

---
+++ BackENd_java/src/main/java/com/backend/backend_java/auth/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.backend.backend_java.auth.controller;
+
+import com.backend.backend_java.auth.dto.request.SignupRequest;
+import com.backend.backend_java.auth.service.SignupService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final SignupService signupService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/signup")
+    public void signup(@RequestBody @Valid SignupRequest request) {
+        signupService.signup(request);
+    }
+}

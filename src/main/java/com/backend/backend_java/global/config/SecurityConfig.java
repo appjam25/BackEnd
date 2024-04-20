9769664da4f8fc86141512964cfc8fcef94e3ea8
dkflfkd53 <b57062562@gmail.com>
2024-04-21 02:12:20 +0900

    Security 추가

--- src/main/java/com/backend/backend_java/global/config/SecurityConfig.java
+++ src/main/java/com/backend/backend_java/global/config/SecurityConfig.java
@@ -3,6 +3,10 @@ package com.backend.backend_java.global.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -10,75 +14,36 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.Arrays;
 import java.util.List;
 
+@EnableWebSecurity
 @RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
 
-//    private final JwtTokenProvider jwtTokenProvider;
-//
-//    private final ObjectMapper objectMapper;
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//    @Bean
-//    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//    @Bean
-//    protected SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
-//
-//        httpSecurity
-//                .csrf(AbstractHttpConfigurer::disable)
-//
-//                .cors(cors -> cors
-//                        .configurationSource(corsConfigurationSource())
-//                )
-//
-//                .headers(headers -> headers
-//                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin
-//                        )
-//                )
-//
-//                .sessionManagement(sessionManagement -> sessionManagement
-//                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                )
-//
-//                .authorizeHttpRequests(authorize -> authorize
-//
-//                        .requestMatchers("/auth/**", "/main", "/club/info/**", "/user/all", "/main/banner",
-//                                "/image", "/notice/all", "/notice/info/**", "/inquiry/create", "/notice/search-notice/**")
-//                        .permitAll()
-//
-//                        .requestMatchers("/notice/apply/**", "/report/apply/**", "/report/cancel/**", "/report-info/**",
-//                                "/interview/choose-time", "/interview/query-time", "/notice/select")
-//                        .hasAnyRole("INDEPENDENT", "ADMIN")
-//
-//                        .requestMatchers("/report/applicant/**", "/report/query-memo/**", "/report/modify-memo", "/question/query/**")
-//                        .hasAnyRole("CLUB_MEMBER", "CLUB_LEADER", "ADMIN")
-//
-//                        .requestMatchers("/club/modify", "/notice/create", "/notice/update", "/notice/delete/**", "/question/answer", "/notice/add-quest", "/notice/all-quest/**", "/alarm/cancel", "/question/delete/**",
-//                                "notice/delete-noticeQuest/**", "/mess/create", "/mess/delete/**", "/alarm/interview-result", "/interview/modify-time", "/interview/club-time/**", "/interview/delete-time/**")
-//                        .hasAnyRole("CLUB_LEADER", "ADMIN")
-//
-//                        .requestMatchers("/admin-club/create", "/admin-club/delete/**", "/admin-club/page",
-//                                "/admin-club/edit-member", "/mess/accept/**", "/mess/all"
-//                        ).hasRole( "ADMIN")
-//
-//                        .requestMatchers("/announcement/create")
-//                        .hasRole("REVERIE")
-//
-//                        .anyRequest().authenticated()
-//                )
-//
-//                .with(new FilterConfig(jwtTokenProvider, objectMapper), Customizer.withDefaults());
-//
-//        return httpSecurity.build();
-//
-//    }
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
+
+        return httpSecurity
+                .csrf().disable()
+                .cors().and()
+                .exceptionHandling()
+
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeRequests()
+                .anyRequest().permitAll()
+
+                .and()
+                .build();
+
+    }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {

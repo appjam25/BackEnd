d0b4463d06e51007d5b0860458fdd31f27abd3f9
도경 <dokyungkim2926@gmail.com>
2024-04-20 21:39:12 +0900

    add :: domain

---
+++ src/main/java/com/backend/backend_java/feed/domain/Feed.java
@@ -0,0 +1,34 @@
+package com.backend.backend_java.feed.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Feed {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String title;
+
+    private String content;
+
+    private String imageUrl;
+
+    private String tag;
+
+    private String place;
+
+    private Integer people;
+}
